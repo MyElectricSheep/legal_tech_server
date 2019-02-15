5c6ffb98e41d5176e4028b9b4fc809f82801a680
Electric_Sheep <iamthebou@gmail.com>
2019-02-15 20:02:12 +0100

    changed sorting algo for recap.js

--- controllers/recap.js
+++ controllers/recap.js
@@ -225,10 +225,10 @@ module.exports = {
           for (let j = 0; j < myFinalAlgoResult.length; j++) {
             if (superSorted[i] === myFinalAlgoResult[j].facture[0].date_debut) {
               myFinalAlgoResultSorted.push({
-                [numberFacture + i]: myFinalAlgoResult[j].facture
+                [numberFacture + i]: myFinalAlgoResult[j]
               });
               myFinalAlgoResultSortedNoNumber.push({
-                facture: myFinalAlgoResult[j].facture
+                facture: myFinalAlgoResult[j]
               });
             }
           }
